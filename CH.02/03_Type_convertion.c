4b1dfb23cb2314f925eb1b49572a1110a1f490a1
Vaibhav koli <vaibhav212159c@gmail.com>
2026-07-31 17:22:32 +0530

    add type convertion

---
+++ CH.02/03_Type_convertion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 9; // int or int ke bich me airthmetic ata h tho hamesa int hi at h
+    int b = 2;
+    float c = a / b;
+    int d = 9.56;
+    printf("the value of a /b is %f\n", c);
+    printf("the value of a /b is %d", d);//hare 9.56 become 9 cause this ist able to store float
+
+    return 0;
+} 
