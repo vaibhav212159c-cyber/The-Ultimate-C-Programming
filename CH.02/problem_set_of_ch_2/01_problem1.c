f658c0bb6795943c7e5dbc1b53815582b8997d95
Vaibhav koli <vaibhav212159c@gmail.com>
2026-08-05 01:00:52 +0530

    Add problem set files for Chapter 2

--- CH.02/problem_set_of_ch_2/01_problem1.c
+++ CH.02/problem_set_of_ch_2/01_problem1.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
-    int a;
-
-    printf("enter the first number :");
-    
-    scanf("%d",&a);
-printf("The remainder when divided by 97 is %d", a % 97);
+    // Which of the following is invalid in C?
+    int a = 1;
+    int b = a;
+    int v = 3 * 3;
+    // char dt = '21 dec 2020'; // Wrong!
     return 0;
 }
---
+++ CH.02/problem_set_of_ch_2/03_problem3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a;
+
+    printf("enter the first number :");
+    
+    scanf("%d",&a);
+printf("The remainder when divided by 97 is %d", a % 97);
+    return 0;
+}
