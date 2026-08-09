b5e30e268cc250a1c8418ec0bc648d6845ae4d78
Vaibhav koli <vaibhav212159c@gmail.com>
2026-08-09 15:52:17 +0530

    Add  conditionals examples

---
+++ CH.03/01_conditionals.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    //int age =5; // don't do anything because age = 5 and if statement only run if age is greter then 10 
+    int age = 15;// run the printf function 
+    if (age>10){
+        printf("your age is grether then 10");
+    }
+    return 0;
+}
---
+++ CH.03/02_if_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    if (age >= 10)
+    {
+        printf("your age is grether then 10");
+    }
+    else
+    {
+        printf("the age is less then 10");
+    }
+    return 0;
+}
